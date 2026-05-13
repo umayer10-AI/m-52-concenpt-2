e6d557e3c50b823a250a94f148ac14d4affd7e87
umayer10-AI <mdumayer10@gmail.com>
2026-05-13 08:13:23 +0600

    add

--- app/src/component/Nav2.jsx
+++ app/src/component/Nav2.jsx
@@ -1,10 +1,33 @@
+"use client"
+import { Button } from '@heroui/react';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 import React from 'react';
 import { FaRegUser } from 'react-icons/fa';
 
 const Nav2 = () => {
+
+    const p = usePathname()
+
     return (
-        <div className='font-semibold'>
+        <div className='font-semibold flex items-center gap-5'>
             <h2 className='flex items-center gap-1'><FaRegUser />Profile</h2>
+            <div className='flex items-center gap-5'>
+                <Link href={'login'}>
+                     {
+                        p==='/login'?
+                        <Button size='sm' className={'bg-linear-to-r from-orange-400 to-red-600'}>Login</Button>
+                        : <button>Login</button>
+                     }
+                </Link>
+                <Link href={'signup'}>
+                     {
+                        p==='/signup'?
+                        <Button size='sm' className={'bg-linear-to-r from-orange-400 to-red-600'}>Sign Up</Button>
+                        : <button>Sign Up</button>
+                     }
+                </Link>
+            </div>
         </div>
     );
 };
