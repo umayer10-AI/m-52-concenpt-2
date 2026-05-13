4c6b64dd16d4262af82b5c619cf96e2731ab2f03
umayer10-AI <mdumayer10@gmail.com>
2026-05-13 08:43:54 +0600

    add

--- server/index.js
+++ server/index.js
@@ -1,6 +1,12 @@
 const express = require('express')
 const app = express()
+const dotenv = require('dotenv')
+const cors = require('cors')
 const port = process.env.PORT || 5000
+dotenv.config()
+
+app.use(cors())
+app.use(express.json())
 
 app.get('/', (req, res) => {
   res.send('Hello World Umayer')
