e5ba8c1ab466a2ad924218aedef52dd792218dc8
umayer10-AI <mdumayer10@gmail.com>
2026-05-13 08:52:49 +0600

    add

--- server/index.js
+++ server/index.js
@@ -21,9 +21,15 @@ const client = new MongoClient(uri, {
 
 const run = async () => {
     try {
-        // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
-        // Send a ping to confirm a successful connection
+
+        const db = client.db('practice')
+        const userCollection = db.collection('allData')
+
+        app.get('/destination', async (req,res) => {
+            
+        })
+
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } 
