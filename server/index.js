224e4f40a5d5a46690536fdbd943b8e90b77335a
umayer10-AI <mdumayer10@gmail.com>
2026-05-13 10:03:47 +0600

    add

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const dotenv = require('dotenv')
 const cors = require('cors')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000
 dotenv.config()
 
@@ -29,6 +29,12 @@ const run = async () => {
         app.get('/destination', async (req,res) => {
             const result = await userCollection.find().toArray()
             res.send(result)
+        })
+
+        app.get('/destination/:id', async (req,res) => {
+            const {id} = req.params
+            const result = await userCollection.findOne({_id: new ObjectId(id)})
+            res.json(result)
         })
 
         await client.db("admin").command({ ping: 1 });
