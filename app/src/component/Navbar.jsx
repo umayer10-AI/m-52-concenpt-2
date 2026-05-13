4dc425e06ea3b2d196c55efe83086e8af5901b63
umayer10-AI <mdumayer10@gmail.com>
2026-05-13 07:55:18 +0600

    add

--- app/src/component/Navbar.jsx
+++ app/src/component/Navbar.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Navbar = () => {
     return (
         <div>
-            
+            <h2 className='text-2xl font-bold bg-linear-to-r from-orange-400 to-red-600 bg-clip-text text-transparent w-fit'>Umayer Site</h2>
         </div>
     );
 };
