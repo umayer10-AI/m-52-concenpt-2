e41417b7aed58a8edefcad17679a9e6303a39b4c
umayer10-AI <mdumayer10@gmail.com>
2026-05-13 08:15:23 +0600

    add

--- app/src/component/Navbar.jsx
+++ app/src/component/Navbar.jsx
@@ -4,10 +4,12 @@ import Nav2 from './Nav2';
 
 const Navbar = () => {
     return (
-        <div>
-            <Nav1></Nav1>
-            <h2 className='text-2xl font-bold bg-linear-to-r from-orange-400 to-red-600 bg-clip-text text-transparent w-fit'>Umayer Site</h2>
-            <Nav2></Nav2>
+        <div className='border-b border-b-orange-700'>
+            <div className='w-[90%] mx-auto flex items-center justify-between py-3'>
+                <Nav1></Nav1>
+                <h2 className='text-2xl font-bold bg-linear-to-r from-orange-400 to-red-600 bg-clip-text text-transparent w-fit'>Umayer Site</h2>
+                <Nav2></Nav2>
+            </div>
         </div>
     );
 };
