a9e829cf4ae4cad6027e506ec22186ead522f687
umayer10-AI <mdumayer10@gmail.com>
2026-05-14 10:23:01 +0600

    add

--- app/src/app/add/page.jsx
+++ app/src/app/add/page.jsx
@@ -19,7 +19,6 @@ const AddPage = () => {
             className="p-6 space-y-8 w-1/2 mx-auto border border-pink-500 rounded-xl"
           >
             <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
-              {/* Destination Name */}
               <div className="md:col-span-2">
                 <TextField name="destinationName" isRequired>
                   <Label>Destination Name</Label>
